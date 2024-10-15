b895fbc3ffbf6fb74ff2467fdd65d6e6e2bf87fe
Mathis BOZON <mathis.bozon03@gmail.com>
2024-10-15 09:34:04 +0200

    TP6 Exo2 complet

--- TP6/Exo2/conso.c
+++ TP6/Exo2/conso.c
@@ -11,24 +11,38 @@ int main()
 {
     //récupère les semaphores
     int mutexbuffer = sem_get(1);
+    int mutexqte = sem_get(4);
+    int mutexindex = sem_get(5);
+
     int semwrite = sem_get(2);
     int semread = sem_get(3);
     //récupère la mémoire partagée
     int shmidbuffer = shmget(BUFFERKEY,BUFFERSIZE*sizeof(int),0);
     int* buffer = shmat(shmidbuffer,NULL,0);
-
-    int qte = 0;
+    //récupère la seconde mémoire partagée (la quantité de données dans le tableau)
+    int shmidqte = shmget(QTEKEY,sizeof(int),0);
+    int* qte = shmat(shmidqte,NULL,0);
+    //récupère la troisème mémoire partagée (l'index de la première donnée dans le tableau)
+    int shmidindex = shmget(INDEXKEY,sizeof(int),0);
+    int* index = shmat(shmidindex,NULL,0);
 
     while(1){
         int value;
         P(semread);
         P(mutexbuffer);
-        value = buffer[qte];
-        qte = (qte+1)%BUFFERSIZE;
-        printf("%de recu depuis buffer %d\n", qte, value);
+        P(mutexindex);
+        value = buffer[*index];
+        printf("reçu %d depuis l'index %d\n",value, *index);
+        *index = (*index+1)%BUFFERSIZE;
+        V(mutexindex);
+        P(mutexqte);
+        *qte -= 1;
+        V(mutexqte);
         V(mutexbuffer);
         V(semwrite);
 
     }
     shmdt(buffer);
+    shmdt(qte);
+    shmdt(index);
 }
--- TP6/Exo2/prod.c
+++ TP6/Exo2/prod.c
@@ -12,7 +12,7 @@ int main()
     //récupère les semaphores / mutexs
     int mutexbuffer = sem_get(1);
     int mutexqte = sem_get(4);
-    int mutexindexlow = sem_get(5);
+    int mutexindex = sem_get(5);
     //semaphore pour savoir si l'écriture est disponible
     int semwrite = sem_get(2);
     //semaphore pour savoir si la lecture est disponible
@@ -22,23 +22,28 @@ int main()
     int* buffer = shmat(shmidbuffer,NULL,0);
     //récupère la seconde mémoire partagée (la quantité de données dans le tableau)
     int shmidqte = shmget(QTEKEY,sizeof(int),0);
-    int* qte = shmat(shmidbuffer,NULL,0);
+    int* qte = shmat(shmidqte,NULL,0);
     //récupère la troisème mémoire partagée (l'index de la première donnée dans le tableau)
-    int shmidqte = shmget(QTEKEY,sizeof(int),0);
-    int* qte = shmat(shmidbuffer,NULL,0);
+    int shmidindex = shmget(INDEXKEY,sizeof(int),0);
+    int* index = shmat(shmidindex,NULL,0);
     while(1){
         int random = (rand()%10);
         sleep(random);
         P(semwrite);
         P(mutexbuffer);
-        P(mutexqte)
-        buffer[*qte] = random;
-        *qte = (*qte+1)%BUFFERSIZE;
-        printf("%de push dans buffer %d\n", *qte, random);
+        P(mutexqte);
+        P(mutexindex);
+        buffer[(*index + *qte)%BUFFERSIZE] = random;
+        printf("envoyé %d sur l'index %d\n", random, (*index + *qte)%BUFFERSIZE);
+        V(mutexindex);
+        *qte = *qte+1;
+        // printf("%de push dans buffer %d\n", *qte, random);
         V(mutexqte);
         V(mutexbuffer);
         V(semread);
 
     }
     shmdt(buffer);
+    shmdt(qte);
+    shmdt(index);
 }
