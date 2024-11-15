0a5792de405b6ff89a3f95e17569a209f79b79a0
MathisBzn <mathis.bozon03@gmail.com>
2024-11-15 16:13:53 +0100

    Exo2 fonctionnel en thread

--- TP9/Exo2/pi_multithread.c
+++ TP9/Exo2/pi_multithread.c
@@ -6,49 +6,45 @@
 
 
 int N;
+int nbThreads;
 //nombre de d'essais qui sont dans le cercle
 int qteHit = 0;
-int nbThreads = 4;
-
-//Simple fonction qui monte un float à sa puissance 2
-float carre(float n){
-    return n*n;
-}
-
-// struct args
-// {
-//     // pthread_mutex_t mutex;
-// };
 
 void *calc(void *args){
+    //récupère le mutex
     pthread_mutex_t *mutex = args;
+    //static initialise la variable une seule fois, elle est partagée ensuite entre tous les threads.
+    //
+    static int th_id = 0;
+    pthread_mutex_lock(mutex);
+    th_id++;
+    pthread_mutex_unlock(mutex);
     int hit = 0;
     //définit les deux nombre flotants qui seront utilisés pour les coordonnées
     float x,y;
     int it = N/nbThreads;
+    uint seed = getpid() + th_id;
     for(int i = 0; i<it; i++){
-        x = ((float)rand()/(float)(RAND_MAX));
-        y = ((float)rand()/(float)(RAND_MAX));
-        //x=carre(x);
-        //y=carre(y);
+        x = ((float)rand_r(&seed)/(float)(RAND_MAX));
+        y = ((float)rand_r(&seed)/(float)(RAND_MAX));
         if(x*x+y*y<1){
             hit++;
         }
     }
-    puts("end calc");
+    //Ajoute le compte local au compte global
     pthread_mutex_lock(mutex);
     qteHit += hit;
     pthread_mutex_unlock(mutex);
-    puts("end");
 
     return NULL;
 }
 
 int main(int argc, char **argv){
-    if(argc < 2){
+    if(argc < 3){
         exit(-1);
     }
     N = atoi(argv[1]);
+    nbThreads = atoi(argv[2]);
     //définit la seed du random sur le time
     srand(time(NULL));
     //création des threads
