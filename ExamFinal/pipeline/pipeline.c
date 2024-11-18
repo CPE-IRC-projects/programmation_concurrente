9b2670ef1377d7615f618cdba387669fe8886070
MathisBzn <mathis.bozon03@gmail.com>
2024-11-18 12:08:28 +0100

    exo4 commenté

--- ExamFinal/pipeline/pipeline.c
+++ ExamFinal/pipeline/pipeline.c
@@ -13,7 +13,9 @@ int main()
     }
     if(fork()==0){
         close(tube[0]);
+        //ouvre le fichier server.log en lecture
         int input = open("./server.log",O_RDONLY);
+        //redirige l'entrée du fichier dans l'entrée standard
         dup2(input, 0);
         dup2(tube[1], 1);
         close(tube[1]);
@@ -26,8 +28,8 @@ int main()
             perror("Création Pipe2");
             exit(-1);
         }
+        close(tube[1]);
         if(fork()==0){
-            close(tube[1]);
             close(tube2[0]);
             dup2(tube[0], 0);
             dup2(tube2[1], 1);
@@ -36,10 +38,12 @@ int main()
             execlp("cut", "cut", "-d,", "-f3", NULL);
         }else{
             close(tube[0]);
-            close(tube[1]);
             close(tube2[1]);
+            //ouvre le fichier de sortie en écriture, le crée si il n'existe pas et le vide
             int input = open("./sortie",O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            //redirige la sortie standard dans le fichier
             dup2(input, 1);
+            //redirige l'entrée standard dans le tube
             dup2(tube2[0], 0);
             close(tube2[0]);
             execlp("sort","sort", NULL);
